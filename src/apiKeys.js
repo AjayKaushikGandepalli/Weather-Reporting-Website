814bd171afdd0ec557d7e3431bd7fbdbff3f9e5c
AjayKaushikGandepalli <gandepalliajaykaushik@gmail.com>
2023-07-21 00:52:14 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "e8ec8f0748757649399720ccd5f97b01",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
